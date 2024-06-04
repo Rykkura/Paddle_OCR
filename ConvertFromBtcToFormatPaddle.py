6f7ef138254965e7a15feb475cf190bab4775c2e
Rykkura <vutrung272002@gmail.com>
2024-06-05 05:23:25 +0700

    fix path

--- ConvertFromBtcToFormatPaddle.py
+++ ConvertFromBtcToFormatPaddle.py
@@ -5,7 +5,7 @@ import pandas as pd
 import json
 import glob
 
-root_path = glob.glob('/content/drive/MyDrive/PaddleOCR/vietnamese_original/vietnamese/labels/*')
+root_path = glob.glob('./vietnamese_original/vietnamese/labels/*')
 
 train_label = open("train_label.txt","w")
 test_label = open("test_label.txt","w")
